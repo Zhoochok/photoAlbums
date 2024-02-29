85e38c3f858804718b17650c21a2c5faa1f17c60
ChingizGalimov <chingizwp88@gmail.com>
2024-02-29 13:38:24 +0300

    addPhoto

--- routes/index.router.js
+++ routes/index.router.js
@@ -8,10 +8,13 @@ const authView = require('./views/auth.view');
 // //Api
 // const productApi = require('./api/product.api');
 const authApi = require('./api/auth.api');
+const photoApiRouter = require('./api/photos.api')
 
 //router Views
 router.use('/', mainRouter);
 router.use('/auth', authView);
 router.use('/api/auth', authApi);
+router.use('/api/photos', photoApiRouter)
+
 
 module.exports = router;
